bdf8af3bc39e62329f98ce64ffde0656bf4a9cb2
Mikhail Khromov <khromov05@gmail.com>
2022-08-25 02:47:43 +0300

    бля дь........

--- maps/admin.py
+++ maps/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+
 from .models import TransportLocation
 
 
 class TransportLocationAdmin(admin.ModelAdmin):
     model = TransportLocation
     fieldsets = ((None, {
-        'fields': ('username', 'email', 'image')
+        'fields': ('station_name', 'transport_line_id', 'transport_location_type',
+                   'morning_thousands_avg_people_per_hour', 'evening_thousands_avg_people_per_hour',
+                   'max_thousands_people_per_hour')
     }),)
     list_display = ('station_name', 'transport_line_id', 'transport_location_type',
                     'morning_thousands_avg_people_per_hour', 'evening_thousands_avg_people_per_hour',
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,31 +1,84 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidmVyeWJpZ3NhZCIsImEiOiJjbDc4MTUzcmEwNWV1NDFveDB2a3l3eGxzIn0.-En_lmcLWHl0K-udYl5gwQ';
+
+
+// создание карты + рисовалки на карте
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [37.56809243178864, 55.773323644594456], // starting position [lng, lat]
     zoom: 15, // starting zoom
-    language: 'ru',
     pitchWithRotate: false
 });
 
 const draw = new MapboxDraw({
     displayControlsDefault: false,
-// Select which mapbox-gl-draw control buttons to add to the map.
     controls: {
         polygon: true,
         trash: true
     },
-// Set mapbox-gl-draw to draw by default.
-// The user does not have to click the polygon control button first.
     defaultMode: 'draw_polygon'
 });
 map.addControl(draw);
 
+// привязка ивентов для подсчета говна слева при апдейте рисунка
 map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
 
+
+
+// мы ждем 4 секунды чтобы быть уверенными что карта вгрузилась и запрашиваем список остановок и на их местах рисуем говно
+setTimeout(() => {
+    console.log('loaded')
+    fetch('/api/v1/TransportLocation?type=json').then((resp) => {
+        resp.json().then((data) => {
+            console.log(data)
+
+            let features = []
+            for (let i = 0; i < data.length; i++) {
+                features.push({
+                    'type': 'Feature',
+                    'properties': {
+                        'description': 'Здесь',
+                        // TODO сюда иконку ебануть
+                        // 'icon': 'theatre-15'
+                        'icon': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [data[i].latitude, data[i].longitude]
+                    }
+                })
+            }
+            map.addSource('places', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': features
+                }
+            })
+
+            // добавляем слой на котором они могут быть прорисованы
+            map.addLayer({
+                'id': 'places',
+                'type': 'symbol',
+                'source': 'places',
+                'layout': {
+                    'icon-image': '{icon}',
+                    'icon-allow-overlap': true
+                }
+            });
+
+        })
+        ;
+        // resp - список значений TransportLocation, где longitude и latitude - наши координаты
+        // крч надо это на карту залить
+
+    })
+}, 4000);
+
 function getAllCoordinates() {
+    // возвращает список списков пар координат (все полигоны)
     let features = draw.getAll().features
     let polygons = [];
     for (let i = 0; i < features.length; i++) {
@@ -39,9 +92,6 @@ function updateArea(e) {
     const answer = document.getElementById('calculated-area');
     if (data.features.length > 0) {
         let polygon_count = getAllCoordinates().length;
-        // const area = turf.area(data);
-// Restrict the area to 2 decimal points.
-//         const rounded_area = Math.rou//nd(area * 100) / 100;
         answer.innerHTML = `<p>У вас ${polygon_count} здания общей площадью area</p>`;
     } else {
         answer.innerHTML = '';
@@ -49,3 +99,6 @@ function updateArea(e) {
             alert('Click the map to draw a polygon.');
     }
 }
+
+
+// points stuff
