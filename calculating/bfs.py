ddbb26f48d8ce4a6397abbc1508a8c6d6c2885f5
maVovk <MichaelVovk@yandex.ru>
2022-08-26 12:30:36 +0300

    готовая апи, не алгос немного сломан)

--- calculating/bfs.py
+++ calculating/bfs.py
@@ -13,7 +13,8 @@ def bfs(node, graph, current_load, initial_load, limit_load):
     queue=[node]
     visited = []
 
-    additional_load[int(node)] = current_load
+    additional_load = [0 for _ in range(len(initial_load) + 1)]
+    additional_load[node] = current_load
     visited.append(node)
     # queue.append(node)
     while queue:
@@ -24,43 +25,40 @@ def bfs(node, graph, current_load, initial_load, limit_load):
             all = 0
 
             for neighbour in graph[s]:
-                all += intial_load[int(neighbour)]
+                all += initial_load[int(neighbour)]
 
             for neighbour in graph[s]:
                 if neighbour not in visited:
                     #print("Сумма:"+str(all))
-                    additional_load[int(neighbour)] += round(additional_load[int(s)] * (intial_load[int(neighbour)] / all), 2)
+                    print(s)
+                    additional_load[int(neighbour)] += round(additional_load[int(s)] * (initial_load[int(neighbour)] / all), 2)
                     visited.append(neighbour)
                     queue.append(neighbour)
 
         else:
             for neighbour in graph[s]:
                 if neighbour not in visited:
+                    print(neighbour)
                     additional_load[int(neighbour)] += additional_load[int(s)]
                     visited.append(neighbour)
                     queue.append(neighbour)
 
-    additional_load = additional_load[1:]
-    intial_load = intial_load[1:]
-    limit_load = limit_load[1:]
-    return additional_load, intial_load, limit_load
+    return additional_load
 
-def bfs_total(graph, initial_load, limit_load):
+def bfs_total(graph, addition, initial_load, limit_load):
     check = set()
-    for i in range(len(graph)):
-        for n in graph[str(i)]:
+    for i in graph.keys():
+        for n in graph[i]:
             if n in check:
-                graph[str(i)].remove(n)
+                graph[i].remove(n)
             else:
                 check.add(n)
                 
-    intial_load = [20, 5, 10, 20, 2, 4, 2] #Базовые нагрузки
-    additional_load = [0, 0, 0, 0, 0, 0, 0] #Дополнительная нагрузка на каждую дорогу(в конце индекс 0 будет обрезан)
-    limit_load = [3, 4, 5, 6, 10] #Сюда поставить лимиты для путей
+    # intial_load = [20, 5, 10, 20, 2, 4, 2] #Базовые нагрузки
+    # additional_load = additional_load = [0 for _ in range(len(initial_load))] #Дополнительная нагрузка на каждую дорогу(в конце индекс 0 будет обрезан)
+    # limit_load = [3, 4, 5, 6, 10] #Сюда поставить лимиты для путей
 
-    current_load = additional_load
-
-    return bfs('0', graph, current_load, initial_load, limit_load)
+    return bfs(0, graph, addition, initial_load, limit_load)
     # visited = [] # List to keep track of visited nodes.
     # queue = []   # Initialize a queue
 
--- calculating/views.py
+++ calculating/views.py
@@ -4,7 +4,7 @@ import json
 import time
 import numpy
 import math
-from bfs import bfs_total
+from calculating.bfs import bfs_total
 
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -19,15 +19,24 @@ def calculate(request):
     # получает данные о зданиях и из них считает все нужное
     if request.method == 'POST':
         data = json.loads(request.body)
-        response = send_dummies()
+        # response = send_dummies()
         # print(data['coordinates'])
         # print(data['0'])
-        start_point_id = calculate_additional_traffic(data)
-        build_graph(start_point_id)
-
+        start_point_id, metro_growth, cars_growth = calculate_additional_traffic(data)
+        graph = build_graph(start_point_id)
+        roads_info = get_roads_info()
 
         # print(start_point_id)
-        
+        # print(metro_growth, cars_growth)
+        # print(graph)
+        # print(roads_info)
+
+        result = bfs_total(graph, cars_growth, roads_info['base_traffic'], roads_info['traffic_limit'])
+        response = make_response(result[1:], roads_info, metro_growth)
+
+        # print(response)
+        # print(start_point_id)
+
     return JsonResponse(response)
 
 
@@ -39,7 +48,7 @@ def calculate_additional_traffic(data):
     centroid = find_centroid(data['coordinates'][0])
     start_point_id = get_nearest_point_id(centroid)
 
-    return start_point_id
+    return start_point_id, metro, cars_growth
 
 
 def calculate_people_growth(data):
@@ -58,6 +67,22 @@ def calculate_cars_growth(bus_enjoyers, car_fans):
     return math.ceil(bus_enjoyers / 84) + math.ceil(car_fans / 1.2)
 
 
+def get_roads_info():
+    data = {
+        'ids': [],
+        'base_traffic': [],
+        'traffic_limit': []
+    }
+    roads_data = Road.objects.all()
+
+    for road in roads_data:
+        data['ids'].append(road.id)
+        data['base_traffic'].append(road.base_traffic)
+        data['traffic_limit'].append(road.traffic_limit)
+    return data
+ 
+
+
 def send_dummies():
     data = {}
     roads_data = Road.objects.all()
@@ -72,10 +97,28 @@ def send_dummies():
     return data
 
 
+def make_response(additional_traffic, road_data, metro_growth):
+    response = {
+        'metro_growth': metro_growth,
+        'roads': {}
+    }
+
+    for i in range(len(additional_traffic)):
+        # print(road_data['ids'][i])
+        response['roads'][ road_data['ids'][i] ] = {
+            'additional_traffic': additional_traffic[i],
+            'base_traffic': road_data['base_traffic'][i],
+            'traffic_limit': road_data['traffic_limit'][i],
+            'direction': 0,
+        }
+    return response
+    # for 
+
+
 def build_graph(start_id):
     graph = dict()
     base_roads = DrawingPoint.objects.get(id=start_id).road.all()
-    graph[0] = [el.id for el in base_roads]
+    graph[0] = [(el.id - 8) for el in base_roads]
     
     queue = graph[0].copy()
     visited = set()
@@ -94,10 +137,9 @@ def build_graph(start_id):
                 if road.id not in visited:
                     queue.append(road.id)
 
-                connections.append(road.id)
+                connections.append(road.id - 8)
 
         graph[road_id] = connections.copy() 
         visited.add(road_id)
-        # print(graph)
 
     return graph
